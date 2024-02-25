10d98e466376fe87d0bd696ef2e7110a89c46cae
HansLuft778 <hendrik.lante@gmail.com>
2024-02-25 01:56:08 +0100

    added 10gb savety

--- src/bestServer.ts
+++ src/bestServer.ts
@@ -78,7 +78,7 @@ export function getBestServer(ns: NS): string {
 }
 
 export function getBestHostByRam(ns: NS): Server[] {
-    let home: Server = { name: "home", maxRam: ns.getServerMaxRam("home"), score: 0 };
+    let home: Server = { name: "home", maxRam: ns.getServerMaxRam("home") - 10, score: 0 };
     let allHosts = getBestServerListCheap(ns, false).filter((server) => {
         return server.maxRam > 2;
     });
