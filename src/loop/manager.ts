8f7e83000ec15521a7e794f1dfd9417f5b6759b1
HansLuft778 <hendrik.lante@gmail.com>
2024-02-25 17:12:44 +0100

    use color enum + merged grow+weak for prepare

--- src/loop/manager.ts
+++ src/loop/manager.ts
@@ -1,54 +1,51 @@
 import { NS } from "@ns";
 
-import { nukeAll } from "../lib.js";
+import { Colors, nukeAll } from "../lib.js";
 import { getBestServerListCheap } from "../bestServer.js";
 import { weakenServer } from "./weakenAlgo.js";
 import { growServer } from "./growingAlgo.js";
 import { hackServer } from "./hackingAlgo.js";
+import { prepareServer } from "./prepareServer.js";
 
-// Text color
-const reset = "\x1b[0m";
-const black = "\x1b[30m";
-const red = "\x1b[31m";
-const green = "\x1b[32m";
-const yellow = "\x1b[33m";
-const blue = "\x1b[34m";
-const magenta = "\x1b[35m";
-const cyan = "\x1b[36m";
-const white = "\x1b[37m";
+let lastTarget = "";
 
 export async function main(ns: NS) {
     ns.tail();
     ns.disableLog("ALL");
 
-    let lastTarget = "";
     // steps: WGWH-WGWH-..
     while (true) {
-        // find the server with the most available money
-        let target: string = getBestServerListCheap(ns, true)[0].name;
-        ns.print("target: " + target);
-
-        if (lastTarget != target) {
-            nukeAll(ns);
-            ns.print("found new best Server: " + target);
-        }
-        lastTarget = target;
-
-        // Debug
-        // target = "n00dles";
-
-        ns.print(cyan + "------------ WEAKENING ------------" + reset);
-        await weakenServer(ns, target);
-
-        ns.print(cyan + "------------- GROWING -------------" + reset);
-        await growServer(ns, target);
-
-        ns.print(cyan + "------------ WEAKENING ------------" + reset);
-        await weakenServer(ns, target);
-
-        ns.print(cyan + "------------- HACKING -------------" + reset);
-        await hackServer(ns, target, 0.8);
+        await loopCycle(ns);
     }
+}
+
+export async function loopCycle(ns: NS) {
+    // find the server with the most available money
+    let target: string = getBestServerListCheap(ns, true)[0].name;
+    ns.print("target: " + target);
+
+    if (lastTarget != target) {
+        nukeAll(ns);
+        ns.print("found new best Server: " + target);
+    }
+    lastTarget = target;
+
+    // target = "n00dles";
+
+    // ns.print(cyan + "------------ WEAKENING ------------" + reset);
+    // await weakenServer(ns, target);
+
+    // ns.print(cyan + "------------- GROWING -------------" + reset);
+    // await growServer(ns, target);
+
+    // ns.print(cyan + "------------ WEAKENING ------------" + reset);
+    // await weakenServer(ns, target);
+
+    ns.print(Colors.cyan + "------------ PREPARING ------------" + Colors.reset);
+    await prepareServer(ns, target);
+
+    ns.print(Colors.cyan + "------------- HACKING -------------" + Colors.reset);
+    await hackServer(ns, target, 0.8);
 }
 
 /**
---
+++ src/loop/prepareServer.ts
@@ -0,0 +1,72 @@
+import { NS } from "@ns";
+import { getBestHostByRam } from "../bestServer";
+import { Colors, getGrowThreads, getWeakenThreadsEff } from "@/lib";
+import { printServerStats } from "@/serverStats";
+
+export async function main(ns: NS) {
+    ns.tail();
+    await prepareServer(ns, "foodnstuff");
+}
+
+export async function prepareServer(ns: NS, target: string) {
+    const safetyMarginMs = 200;
+
+    const weakenScriptRam = 1.75;
+    const growingScriptRam = 1.75;
+
+    const allHosts = getBestHostByRam(ns);
+
+    while (true) {
+        const targetWeakenThreads = getWeakenThreadsEff(ns, target);
+
+        ns.print(Colors.cyan + "------------ WEAKENING ------------" + Colors.reset);
+        ns.print("total weaken threads needed: " + targetWeakenThreads);
+        // weaken to min sec lvl
+        let sumThreadsDone = 0;
+        while (sumThreadsDone < targetWeakenThreads) {
+            const weakenTime = ns.getWeakenTime(target);
+            for (let i = 0; i < allHosts.length; i++) {
+                if (sumThreadsDone >= targetWeakenThreads) break;
+
+                const host = allHosts[i];
+                const freeRam = host.maxRam - ns.getServerUsedRam(host.name);
+
+                const numThreadsOnHost = Math.floor(freeRam / weakenScriptRam);
+
+                ns.exec("weaken.js", host.name, numThreadsOnHost, target);
+                sumThreadsDone += numThreadsOnHost;
+            }
+            await ns.sleep(weakenTime + safetyMarginMs);
+            ns.print("done with " + sumThreadsDone + "/" + targetWeakenThreads + " weakens");
+        }
+        printServerStats(ns, target, 0.8);
+        const targetGrowThreads = getGrowThreads(ns, target);
+        // check if grow is needed
+        if (targetGrowThreads === 0) {
+            ns.print("No growth needed");
+            break;
+        }
+
+        ns.print(Colors.cyan + "------------- GROWING -------------" + Colors.reset);
+        ns.print("total growing threads needed: " + targetGrowThreads);
+        // grow one batch
+        const growingTime = ns.getGrowTime(target);
+        let sumGrowthThreadsDone = 0;
+        for (let i = 0; i < allHosts.length; i++) {
+            if (sumGrowthThreadsDone >= targetGrowThreads) break;
+
+            const host = allHosts[i];
+            const freeRam = host.maxRam - ns.getServerUsedRam(host.name);
+
+            const numThreadsOnHost = Math.floor(freeRam / growingScriptRam);
+
+            ns.exec("grow.js", host.name, numThreadsOnHost, target);
+            sumGrowthThreadsDone += numThreadsOnHost;
+        }
+        await ns.sleep(growingTime + safetyMarginMs);
+        ns.print("done with " + sumGrowthThreadsDone + "/" + targetGrowThreads + " grows");
+        printServerStats(ns, target, 0.8);
+    }
+
+    ns.print("Done preparing!");
+}
