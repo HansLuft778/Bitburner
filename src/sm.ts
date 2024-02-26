d14ad5af1eeb280e639234770d0e86cf799d48f0
HansLuft778 <hendrik.lante@gmail.com>
2024-02-27 00:13:22 +0100

    closes update `sm.js` #12

--- src/sm.ts
+++ src/sm.ts
@@ -1,73 +1,91 @@
 import { NS } from "@ns";
 
 export async function main(ns: NS) {
-    let upgrade = false;
-    let upgradeName = '';
+    let isUpgrade = false;
+    let upgradeName = "";
     let upgradeRam = 0;
 
-    let buy = false;
-    let buyName = '';
+    let isBuy = false;
+    let buyName = "";
     let buyRam = 0;
+    let buyUnit = "";
 
     if (ns.args.length == 3 || ns.args.length == 0) {
-        if (ns.args[0] == '-u') {
-            upgrade = true;
+        if (ns.args[0] == "-u") {
+            isUpgrade = true;
             upgradeName = ns.args[1].toString();
             upgradeRam = Number(ns.args[2]);
         }
-        if (ns.args[0] == '-b') {
-            buy = true;
+        if (ns.args[0] == "-b") {
+            isBuy = true;
             buyName = ns.args[1].toString();
-            buyRam = Number(ns.args[2]);
+            buyRam = Number(String(ns.args[2]).slice(0, -1));
+            buyUnit = String(ns.args[2]).slice(-1);
         }
     } else {
-        ns.tprint('\nusage: sm.js [options]\n\nOptions:\n\t-u <Name> <Ram>\n\t-b <Name> <Ram>');
+        ns.tprint("\nusage: sm.js [options]\n\nOptions:\n\t-u <Name> <Ram><G|T|P>\n\t-b <Name> <Ram><G|T|P>");
         return;
     }
 
-    if (upgrade) {
+    if (isUpgrade) {
         // get current ram
         const servers = ns.getPurchasedServers();
         if (!servers.includes(upgradeName)) {
-            ns.tprint('You do not own a server called ' + upgradeName);
+            ns.tprint("You do not own a server called " + upgradeName);
         }
 
         let price = ns.getPurchasedServerUpgradeCost(upgradeName, upgradeRam);
 
         const answer = await ns.prompt(
-            'upgrading the server (' +
+            "upgrading the server (" +
                 upgradeName +
-                ') to ' +
+                ") to " +
                 upgradeRam +
-                'GB of Ram, will cost ' +
+                "GB of Ram, will cost " +
                 ns.formatNumber(price),
         );
         if (answer) ns.upgradePurchasedServer(upgradeName, upgradeRam);
-    } else if (buy) {
+    } else if (isBuy) {
+        buyRam = getGBfromAnyUnit(ns, buyRam, buyUnit);
+        if (buyRam < 1) return;
+
         let price = ns.getPurchasedServerCost(buyRam);
 
         const answer = await ns.prompt(
-            'buying the server (' + buyName + ') with ' + buyRam + 'GB of Ram, will cost ' + ns.formatNumber(price),
+            "buying the server (" + buyName + ") with " + buyRam + "GB of Ram, will cost " + ns.formatNumber(price),
         );
         if (answer) ns.purchaseServer(buyName, buyRam);
     } else {
-        const playerMoney = ns.getServerMoneyAvailable('home');
+        const playerMoney = ns.getServerMoneyAvailable("home");
         let ramSize = 16;
         while (ns.getPurchasedServerCost(ramSize) < playerMoney) {
             ramSize *= 2;
         }
         ramSize = ramSize / 2;
         ns.tprint(
-            'you can buy a server with ' +
+            "you can buy a server with " +
                 ramSize +
-                'GB of ram. This costs ' +
+                "GB of ram. This costs " +
                 ns.formatNumber(ns.getPurchasedServerCost(ramSize)),
         );
 
-        const answer = await ns.prompt('buy Server for ' + ns.formatNumber(ns.getPurchasedServerCost(ramSize)) + '?');
+        const answer = await ns.prompt("buy Server for " + ns.formatNumber(ns.getPurchasedServerCost(ramSize)) + "?");
 
         if (answer) {
-            ns.purchaseServer('hacker', ramSize);
+            ns.purchaseServer("hacker", ramSize);
         }
+    }
+}
+
+function getGBfromAnyUnit(ns: NS, ram: number, unit: string): number {
+    if (unit == "G") {
+        return ram;
+    } else if (unit == "T") {
+        return ram * 1024;
+    } else if (unit == "P") {
+        return ram * 1024 * 1024;
+    } else {
+        ns.tprint("\nusage: sm.js [options]\n\nOptions:\n\t-u <Name> <Ram><G|T|P>\n\t-b <Name> <Ram><G|T|P>");
+        return 0;
     }
 }
