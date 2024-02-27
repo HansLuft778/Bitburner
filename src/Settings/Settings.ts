fea8799f92868b09db61f0699804b9d27d7f9401
HansLuft778 <hendrik.lante@gmail.com>
2024-02-27 22:10:55 +0100

    updated settings

--- src/Settings/Settings.ts
+++ src/Settings/Settings.ts
@@ -1,43 +1,37 @@
-import { NS } from "@ns";
-import { readFile } from "fs";
-
- 
 /**
  * Represents the settings for the application.
- * 
+ *
  * To modify the settings, update the class properties accordingly.
  */
-class Settings {
-
+export class Settings {
     /**
      * The target the daemon should hack, leave empty to hack the best server.
      * Default: ""
      */
-    public target = "";
+    public static target = "";
 
     /**
      * The maximum amount of money the daemon is allowed to use to buy servers.
      * Default: 0
      */
-    public maxMoneyToBuy = 0;
+    public static maxMoneyToBuy = 0;
 
     /**
      * The maximum amount of money the daemon is allowed to hack from a server, leave at 0 to let the daemon decide.
      * Default: 0
      */
-    public hackThreshold = 0;
-}
+    public static hackThreshold = 0;
 
-export async function main(ns: NS) {
-    ns.tail();
-    ns.disableLog("ALL");
+    /**
+     * The maximum amount of RAM the daemon should leave free on the Home server.
+     * Default: 50
+     */
+    public static homeFreeRam = 50;
 
-    const file = "/Settings/settings.txt";
-
-    const data = ns.read(file);
-    ns.print(data)
-
-    const settings: Settings = JSON.parse(data);
-
-    ns.print(settings.hackThreshold);
+    /**
+     * The delay time in milliseconds.
+     * This constant represents the amount of time to add as a margin when calling weak/grow/hack in parallel mode.
+     * Default: 1000
+     */
+    public static DELAY_MARGIN_MS = 1000;
 }
