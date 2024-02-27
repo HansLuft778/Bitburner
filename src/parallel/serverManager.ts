657f6bb00bc40f831c463a30fdd2316b04e8647a
HansLuft778 <hendrik.lante@gmail.com>
2024-02-27 17:08:33 +0100

    refactor serverManager

--- src/parallel/serverManager.ts
+++ src/parallel/serverManager.ts
@@ -1,45 +1,44 @@
 import { NS } from "@ns";
 
-export async function main(ns: NS) {
-    //
-}
+export class ServerManager {
+    static buyServer(ns: NS, ram: number): string {
+        const limit = ns.getPurchasedServerLimit();
+        const all = ns.getPurchasedServers();
+        if (all.length >= limit) {
+            ns.print("[server Manager] attempted to buy a new server, but the limit has been reached");
+            return "";
+        }
 
-export function buyServer(ns: NS, ram: number) {
-    const limit = ns.getPurchasedServerLimit();
-    const all = ns.getPurchasedServers();
-    if (all.length >= limit) {
-        ns.print("[server Manager] attempted to buy a new server, but the limit has been reached");
-        return false;
+        // round ram up to the next power of 2
+        const exponent = Math.ceil(Math.log2(ram));
+        ram = Math.pow(2, exponent);
+
+        const name = ns.purchaseServer("aws", ram);
+        return name;
     }
 
-    const price = ns.getPurchasedServerCost(ram);
-    const name = getNextServerName(ns);
-    ns.purchaseServer(name, ram);
+    static upgradeServer(ns: NS, ram: number, name: string) {
+        // get current ram
+        const servers = ns.getPurchasedServers();
+        if (!servers.includes(name)) {
+            ns.print("[server Manager] attempted to upgrade Server, but the server does not exist");
+            return false;
+        }
 
-    return true;
-}
+        const price = ns.getPurchasedServerUpgradeCost(name, ram);
 
-export function upgradeServer(ns: NS, ram: number, name: string) {
-    // get current ram
-    const servers = ns.getPurchasedServers();
-    if (!servers.includes(name)) {
-        ns.print("[server Manager] attempted to upgrade Server, but the server does not exist");
-        return false;
+        ns.upgradePurchasedServer(name, ram);
+
+        return true;
     }
-
-    const price = ns.getPurchasedServerUpgradeCost(name, ram);
-
-    ns.upgradePurchasedServer(name, ram);
-
-    return true;
 }
 
 function getNextServerName(ns: NS) {
     const allServers = ns.getPurchasedServers();
-    const filteredStrings = allServers.filter((str) => str.startsWith("aws-"));
+    const awsServers = allServers.filter((str) => str.startsWith("aws-"));
 
     // get highest number and construct the new name
-    const numbers = filteredStrings.map((server) => parseInt(server.split("-")[1]));
+    const numbers = awsServers.map((server) => parseInt(server.split("-")[1]));
     const max = Math.max(...numbers);
     const newName = "aws-" + (max + 1);
     return newName;
