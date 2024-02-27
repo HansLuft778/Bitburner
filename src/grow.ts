e1e51eb58b7cb02986dcc42b9f1b4bbef2a25983
HansLuft778 <hendrik.lante@gmail.com>
2024-02-27 17:06:34 +0100

    added delay parameter to base WGH scripts

--- src/grow.ts
+++ src/grow.ts
@@ -1,5 +1,6 @@
 import { NS } from "@ns";
 export async function main(ns: NS) {
-    if (typeof ns.args[0] !== "string") throw new TypeError("First argument must be a string");
-    await ns.grow(ns.args[0]);
+    if (typeof ns.args[0] !== "string") throw new TypeError("[GROW] First argument must be a string");
+    if (typeof ns.args[1] !== "number") throw new TypeError("[GROW] Second argument must be a number");
+    await ns.grow(ns.args[0], { additionalMsec: ns.args[1] });
 }
