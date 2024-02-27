fae5323ce9f607018eafc321cc89c0cb9603af7a
HansLuft778 <hendrik.lante@gmail.com>
2024-02-27 15:54:28 +0100

    rm imports

--- src/hackingDeamon.ts
+++ src/hackingDeamon.ts
@@ -1,9 +1,8 @@
 import { NS } from "@ns";
 import { getBestHostByRam, getBestServerListCheap } from "./bestServer";
 import { Colors, getGrowThreadsThreshold, getWeakenThreadsAfterHack } from "./lib";
-import { loopCycle } from "./loop/manager";
-import { parallelCycle } from "./parallel/manager";
 import { prepareServer } from "./loop/prepareServer";
+import { parallelCycle } from "./parallel/manager";
 
 // TODO: determine the threshold by first prepping the server to max money/min sec lvl.
 // Then find the percentage of money that can be stolen safely, so it can be regrown per one-hit.
