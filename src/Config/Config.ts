b95e00c2dc04630b7dee5a9310338f07584b2a04
HansLuft778 <hendrik.lante@gmail.com>
2024-02-28 16:42:35 +0100

    Add MIN_HACK_THRESHOLD constant to Config class

--- src/Config/Config.ts
+++ src/Config/Config.ts
@@ -29,6 +29,14 @@ export class Config {
     public static readonly THRESHOLD_STEP = 0.05;
 
     /**
+     * The minimum hack threshold value.
+     * This value represents the minimum threshold required for a successful hack.
+     * Default: 0.15
+     */
+    public static readonly MIN_HACK_THRESHOLD = 0.15;
+
+
+    /**
      * The maximum amount of RAM the daemon should leave free on the Home server.
      * Default: 50
      */
