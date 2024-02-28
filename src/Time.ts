05ec3a655a676f6337d72f4aad6aaad70cecbc93
HansLuft778 <hendrik.lante@gmail.com>
2024-02-29 00:48:26 +0100

    Refactor Time class to track sleep time

--- src/Time.ts
+++ src/Time.ts
@@ -3,24 +3,27 @@ import { NS } from "@ns";
 export class Time {
     private static instance: Time;
 
-    startTimeValue = 0;
-    endTimeValue = 0;
+    private startTimeMillis = 0;
+    private endTimeMillis = 0;
+    private sleepTimeMillis = 0;
 
     public startTime() {
-        this.startTimeValue = Date.now();
+        this.sleepTimeMillis = 0;
+        this.startTimeMillis = Date.now();
     }
 
     public endTime() {
-        this.endTimeValue = Date.now();
+        this.endTimeMillis = Date.now();
     }
 
-    public substractTime(time: number) {
-        this.startTimeValue += time;
+    public accumulateSleepTime(time: number) {
+        this.sleepTimeMillis += time;
     }
 
     public getTime(ns: NS) {
-        ns.write("timelog.txt", "Time: " + (this.endTimeValue - this.startTimeValue) + "ms", "a");
-        return this.endTimeValue - this.startTimeValue;
+        const runningTime = this.endTimeMillis - this.startTimeMillis - this.sleepTimeMillis;
+        ns.write("timelog.txt", "Time: " + runningTime + "ms\n", "a");
+        return runningTime;
     }
 
     private constructor() {
