0d016ae61610c7724d9d554eed4ca531e0080096
HansLuft778 <hendrik.lante@gmail.com>
2024-02-29 18:57:32 +0100

    gorg

---
+++ src/gorg.ts
@@ -0,0 +1,20 @@
+import { NS } from "@ns";
+import { getBestHostByRam, getBestHostByRamOptimized } from "./bestServer";
+import { Colors } from "./lib";
+export async function main(ns: NS) {
+    ns.tail();
+    ns.disableLog("ALL");
+
+    const start = Date.now();
+    for (let i = 0; i < 400; i++) {
+        const element = getBestHostByRam(ns);
+    }
+    const end = Date.now();
+    for (let i = 0; i < 400; i++) {
+        const element = getBestHostByRamOptimized(ns);
+    }
+    const end2 = Date.now();
+
+    ns.print(Colors.GREEN + "normal: " + (end - start));
+    ns.print(Colors.GREEN + "optimized: " + (end2 - end));
+}
