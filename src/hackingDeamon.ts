67c58d7c347931b11d97cc1df38d97a3e89f633b
HansLuft778 <hendrik.lante@gmail.com>
2024-03-01 16:33:11 +0100

    merged Threshold functions, closes #26

--- src/hackingDeamon.ts
+++ src/hackingDeamon.ts
@@ -44,7 +44,7 @@ export async function main(ns: NS) {
                 ) {
                     await prepareServer(ns, target);
                 }
-                hackThreshold = getHackThresholdBatch(ns, target);
+                hackThreshold = getHackThreshold(ns, target);
                 ns.tprint(Colors.E_ORANGE + "hackThreshold: " + hackThreshold);
 
                 if (
@@ -98,10 +98,6 @@ export async function main(ns: NS) {
     }
 }
 
-/**
- * Needs update: hack as much as possible, without having to buy a extra server
- *
- */
 function getHackThreshold(ns: NS, target: string): number {
     const allHosts = getBestHostByRamOptimized(ns);
     const totalMaxRam = allHosts.reduce((acc, server) => {
@@ -110,89 +106,34 @@ function getHackThreshold(ns: NS, target: string): number {
 
     let hackThreshold = 0.9;
     const THRESHOLD_STEP = Config.THRESHOLD_STEP;
-    const MIN_HACK_THRESHOLD = Config.MIN_HACK_THRESHOLD;
 
+    // const moneyAllowedToUse = ns.getServerMoneyAvailable("home") * (2 / 3);
     const RAM_WEAKEN = Config.WEAKEN_SCRIPT_RAM;
     const RAM_GROW = Config.GROW_SCRIPT_RAM;
     const RAM_HACK = Config.HACK_SCRIPT_RAM;
+
     while (true) {
-        // how many threads i need to grow the server from (1 - Threshold) to 1
-        // needs threshold grow calculation, cause when the server is at max money, it would return 0 otherwise
-        const serverGrowThreads = getGrowThreadsThreshold(ns, target, hackThreshold + THRESHOLD_STEP);
+        let serverHackThreads = 0;
+        let serverGrowThreads = 0;
+        if (ns.fileExists("Formulas.exe", "home")) {
+            serverHackThreads = getHackThreadsFormulas(ns, target, hackThreshold);
 
-        // how many threads i need to weaken security to 0 after growings
-        const secIncrease = ns.growthAnalyzeSecurity(serverGrowThreads);
-        const secondWeakenThreads = Math.ceil(secIncrease / ns.weakenAnalyze(1));
+            serverGrowThreads = getGrowThreadsFormulas(ns, target, serverHackThreads);
+        } else {
+            const hackAmount = ns.getServerMaxMoney(target) * hackThreshold;
+            serverHackThreads = Math.ceil(ns.hackAnalyzeThreads(target, hackAmount));
 
-        // how many threads i need to hack the server
-        const hackAmount = ns.getServerMaxMoney(target) * hackThreshold;
-        const serverHackThreads = Math.ceil(ns.hackAnalyzeThreads(target, hackAmount));
-
-        // how many to weak to min sec lvl after [threshold]-hack
+            serverGrowThreads = getGrowThreadsThreshold(ns, target, hackThreshold + THRESHOLD_STEP);
+        }
         const firstWeakenThreads = getWeakenThreadsAfterHack(ns, serverHackThreads);
 
-        // this var describes the total amount of threads i need to run parallel mode
-        const totalRamNeeded =
-            firstWeakenThreads * RAM_WEAKEN +
-            serverGrowThreads * RAM_GROW +
-            secondWeakenThreads * RAM_WEAKEN +
-            serverHackThreads * RAM_HACK;
-
-        // log all
-        ns.print("predicted threads needed:");
-        ns.print("firstWeakenThreads: " + firstWeakenThreads);
-        ns.print("serverGrowThreads: " + serverGrowThreads);
-        ns.print("secondWeakenThreads: " + secondWeakenThreads);
-        ns.print("serverHackThreads: " + serverHackThreads);
-
-        if (totalRamNeeded < totalMaxRam) {
-            ns.print(
-                "needs " + totalRamNeeded + "GB of RAM and got " + totalMaxRam + ". Running parallel mode on " + target,
-            );
-            break;
-        }
-        if (hackThreshold < MIN_HACK_THRESHOLD) {
-            ns.tprint("Error! Not enough RAM to run parallel mode on " + target);
-            throw new Error("Error! Not enough RAM to run parallel mode on " + target);
-            // TODO: should run loop more instead
-        }
-        hackThreshold = Math.round((hackThreshold - THRESHOLD_STEP) * 100) / 100;
-        ns.print("Threshold is too high, trying with: " + hackThreshold);
-    }
-    return hackThreshold;
-}
-
-function getHackThresholdBatch(ns: NS, target: string): number {
-    const allHosts = getBestHostByRamOptimized(ns);
-    const totalMaxRam = allHosts.reduce((acc, server) => {
-        return acc + server.maxRam;
-    }, 0);
-
-    let hackThreshold = 0.9;
-    const THRESHOLD_STEP = Config.THRESHOLD_STEP;
-
-    // const MIN_HACK_THRESHOLD = Config.MIN_HACK_THRESHOLD;
-    // const moneyAllowedToUse = ns.getServerMoneyAvailable("home") * (2 / 3);
-
-    while (true) {
-        // how many threads i need to hack the server
-        const serverHackThreads = getHackThreadsFormulas(ns, target, hackThreshold);
-
-        // how many threads i need to grow the server from (1 - Threshold) to 1
-        // needs threshold grow calculation, cause when the server is at max money, it would return 0 otherwise
-        const serverGrowThreads = getGrowThreadsFormulas(ns, target, serverHackThreads);
-
-        // how many threads i need to weaken security to 0 after growings
         const secondWeakenThreads = getWeakenThreadsAfterGrow(ns, serverGrowThreads);
 
-        // how many to weak to min sec lvl after [threshold]-hack
-        const firstWeakenThreads = getWeakenThreadsAfterHack(ns, serverHackThreads);
-
         // this var describes the total amount of threads i need to run parallel mode
-        const weaken1RamNeeded = Config.WEAKEN_SCRIPT_RAM * firstWeakenThreads;
-        const weaken2RamNeeded = Config.WEAKEN_SCRIPT_RAM * secondWeakenThreads;
-        const growRamNeeded = Config.GROW_SCRIPT_RAM * serverGrowThreads;
-        const hackRamNeeded = Config.HACK_SCRIPT_RAM * serverHackThreads;
+        const weaken1RamNeeded = RAM_WEAKEN * firstWeakenThreads;
+        const weaken2RamNeeded = RAM_WEAKEN * secondWeakenThreads;
+        const growRamNeeded = RAM_GROW * serverGrowThreads;
+        const hackRamNeeded = RAM_HACK * serverHackThreads;
 
         const totalRamNeeded = weaken1RamNeeded + weaken2RamNeeded + growRamNeeded + hackRamNeeded;
 
@@ -202,13 +143,6 @@ function getHackThresholdBatch(ns: NS, target: string): number {
         ns.print("serverGrowThreads: " + serverGrowThreads + " with " + growRamNeeded + "GB of RAM");
         ns.print("secondWeakenThreads: " + secondWeakenThreads + " with " + weaken2RamNeeded + "GB of RAM");
         ns.print("serverHackThreads: " + serverHackThreads + " with " + hackRamNeeded + "GB of RAM");
-        ns.print(
-            "totalThreads: " +
-                (firstWeakenThreads + serverGrowThreads + secondWeakenThreads + serverHackThreads) +
-                " with " +
-                totalRamNeeded +
-                "GB of RAM",
-        );
 
         if (totalRamNeeded < totalMaxRam) {
             ns.print(
@@ -223,17 +157,23 @@ function getHackThresholdBatch(ns: NS, target: string): number {
                 ". Attempting to upgrade/buy server...",
         );
 
-        const hackServer = ServerManager.buyOrUpgradeServer(ns, hackRamNeeded, Config.HACK_SERVER_NAME);
-        const Server = ServerManager.buyOrUpgradeServer(ns, growRamNeeded, Config.GROW_SERVER_NAME);
-        const weaken1Server = ServerManager.buyOrUpgradeServer(ns, weaken1RamNeeded, Config.WEAK_SERVER_NAME);
-        const weaken2Server = ServerManager.buyOrUpgradeServer(ns, weaken2RamNeeded, Config.WEAK_SERVER_NAME);
+        if (ns.fileExists("Formulas.exe", "home")) {
+            const hackServer = ServerManager.buyOrUpgradeServer(ns, hackRamNeeded, Config.HACK_SERVER_NAME);
+            const Server = ServerManager.buyOrUpgradeServer(ns, growRamNeeded, Config.GROW_SERVER_NAME);
+            const weaken1Server = ServerManager.buyOrUpgradeServer(ns, weaken1RamNeeded, Config.WEAK_SERVER_NAME);
+            const weaken2Server = ServerManager.buyOrUpgradeServer(ns, weaken2RamNeeded, Config.WEAK_SERVER_NAME);
 
-        if (hackServer !== "" && Server !== "" && weaken1Server !== "" && weaken2Server !== "") {
-            ns.print(Colors.GREEN + "Servers bought, running parallel mode on " + target);
-            return hackThreshold;
+            if (hackServer !== "" && Server !== "" && weaken1Server !== "" && weaken2Server !== "") {
+                ns.print(Colors.GREEN + "Servers bought, running parallel mode on " + target);
+                return hackThreshold;
+            }
         }
 
         hackThreshold = Math.round((hackThreshold - THRESHOLD_STEP) * 100) / 100;
+        if (hackThreshold < Config.MIN_HACK_THRESHOLD) {
+            ns.tprint(Colors.RED + "Error! Not enough RAM to run parallel mode on " + target);
+            throw new Error("Error! Not enough RAM to run parallel mode on " + target);
+        }
         ns.print("Threshold is too high, trying with: " + hackThreshold);
     }
 }
