cc61aab86436d7d88a393885e270b4404aada5da
HansLuft778 <hendrik.lante@gmail.com>
2024-03-12 19:17:38 +0100

    update tea party script to only run once per cycle

--- src/Corporation/teaParty.ts
+++ src/Corporation/teaParty.ts
@@ -3,35 +3,35 @@ import { NS } from "@ns";
 export async function main(ns: NS) {
     ns.tail();
     ns.disableLog("ALL");
-    // ns.corporation.throwParty();
-    // ns.corporation.buyTea("lettuce begin", "Sector-12");
-
-    // ns.corporation.getCorporation();
-    const divisions: string[] = ["Lettuce begin", "BASF"];
+    const corp = ns.corporation;
 
     const COST_PER_EMPOLYEE = 200_000;
     const ENERGY_THRESHOLD = 99;
     const MORALE_THRESHOLD = 99;
 
+    let lastState = "SALE";
     while (true) {
-        // const coorp = ns.corporation.getCorporation();
+        if (lastState !== "SALE") {
+            lastState = await corp.nextUpdate();
+            continue;
+        }
+
+        const divisions = corp.getCorporation().divisions;
 
         for (const division of divisions) {
-            const divisionCities = ns.corporation.getDivision(division).cities;
+            const divisionCities = corp.getDivision(division).cities;
             for (const city of divisionCities) {
-                const office = ns.corporation.getOffice(division, city);
+                const office = corp.getOffice(division, city);
                 const energy = office.avgEnergy;
                 const morale = office.avgMorale;
 
                 ns.print(`City: ${city}, Energy: ${ns.formatNumber(energy)}, Morale: ${ns.formatNumber(morale)}`);
 
-                if (energy < ENERGY_THRESHOLD) ns.corporation.buyTea(division, city);
-                if (morale < MORALE_THRESHOLD) ns.corporation.throwParty(division, city, COST_PER_EMPOLYEE);
+                if (energy < ENERGY_THRESHOLD) corp.buyTea(division, city);
+                if (morale < MORALE_THRESHOLD) corp.throwParty(division, city, COST_PER_EMPOLYEE);
             }
         }
 
-        const state = await ns.corporation.nextUpdate();
-
-        ns.print(state);
+        lastState = await corp.nextUpdate();
     }
 }
