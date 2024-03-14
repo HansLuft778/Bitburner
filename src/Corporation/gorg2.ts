060fd251782cdb98c3806efeb8b9dce4025b32cc
HansLuft778 <hendrik.lante@gmail.com>
2024-03-14 19:17:42 +0100

    gorg

--- src/Corporation/gorg2.ts
+++ src/Corporation/gorg2.ts
@@ -1,5 +1,5 @@
 import { NS } from "@ns";
-import { buyBoostMaterial, getOptimalBoostMaterialQuantities } from "./lib";
+import { ChangeEmployeeRatio, CityName, Ratio, buyBoostMaterial, getOptimalBoostMaterialQuantities } from "./lib";
 
 export async function main(ns: NS) {
     ns.tail();
@@ -7,11 +7,19 @@ export async function main(ns: NS) {
     ns.print("\n");
 
     const corp = ns.corporation;
-    const ad = corp.getHireAdVertCount("Lettuce begin");
-    ns.print(ad);
-    // const quant = getOptimalBoostMaterialQuantities(corp.getIndustryData("Agriculture"), 5250 * 0.8);
-    // ns.print(quant);
-    // const mat = corp.getMaterial("BASF", "Chongqing", "Hardware").stored;
-    // ns.print(mat);
-    // await buyBoostMaterial(ns, "Lettuce begin");
+    const data = corp.getIndustryData("Tobacco");
+    const warehouseSpace = corp.getWarehouse("Tobacco", "Sector-12").size * 0.8;
+    const neededMaterial = getOptimalBoostMaterialQuantities(data, warehouseSpace);
+    ns.print(neededMaterial);
+    const gorg = corp.getProduct("Tobacco", "Sector-12", "gorg22").competition;
+    const gorg1 = corp.getProduct("Tobacco", "Sector-12", "gorg22").demand;
+    ns.print(`competition 22: ${gorg}`);
+    ns.print(`demand 22: ${gorg1}`);
+    const gorg2 = corp.getProduct("Tobacco", "Sector-12", "gorg23").competition;
+    const gorg21 = corp.getProduct("Tobacco", "Sector-12", "gorg23").demand;
+    ns.print(`competition 23: ${gorg2}`);
+    ns.print(`demand 23: ${gorg21}`);
+
+    // await buyBoostMaterial(ns, "KFC");
+    // ChangeEmployeeRatio(ns, "Tobacco", CityName.Sector12, Ratio.profitRatio);
 }
