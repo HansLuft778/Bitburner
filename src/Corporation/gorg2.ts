4fcd75ff1ae6be8b62b17d757866449a3d3a4936
HansLuft778 <hendrik.lante@gmail.com>
2024-08-15 16:21:58 +0200

    added new saves

--- src/Corporation/gorg2.ts
+++ src/Corporation/gorg2.ts
@@ -1,4 +1,5 @@
 import { NS } from "@ns";
+import { developNewProduct } from "./lib";
 
 export async function main(ns: NS) {
     ns.tail();
@@ -7,17 +8,5 @@ export async function main(ns: NS) {
 
     const corp = ns.corporation;
 
-    const currentProducts = corp.getDivision("Tobacco").products;
-    corp.getProduct("Tobacco", "Sector-12", currentProducts[0]).desiredSellPrice = 1e6;
-
-    for (const product of currentProducts) {
-        const demand = corp.getProduct("Tobacco", "Sector-12", product).actualSellAmount;
-        const comp = corp.getProduct("Tobacco", "Sector-12", product).competition;
-        const rating = corp.getProduct("Tobacco", "Sector-12", product).rating;
-        const gorg = corp.getProduct("Tobacco", "Sector-12", product).stats.performance;
-        ns.print(`${product} demand: ${demand}, competition: ${comp}, rating: ${rating}, score: ${gorg}`);
-    }
-
-    // await buyBoostMaterial(ns, "KFC");
-    // ChangeEmployeeRatio(ns, "Tobacco", CityName.Sector12, Ratio.profitRatio);
+    await developNewProduct(ns);
 }
