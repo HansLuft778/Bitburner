e695f031fbdb9863973738ec060ce7cef8ad8e9b
HansLuft778 <hendrik.lante@gmail.com>
2025-01-11 10:50:49 +0100

    delete everything

--- src/HacknetNodes/manager.ts
+++
@@ -1,8 +0,0 @@
-import { NS } from "@ns";
-
-const EXPECTED_TURNOVER_TIME = 1000 * 60 * 60 * 2; // 2 hours
-
-export async function main(ns: NS) {
-    const purchaseCost = ns.hacknet.getPurchaseNodeCost();
-    const stats = ns.hacknet.getNodeStats(0);
-}
