e5d43c67275f48db480d2fe9df84fe4c02bd027b
HansLuft778 <hendrik.lante@gmail.com>
2025-05-14 14:56:32 +0200

    fix: handle import error for go_types.State by providing fallback type

--- http_server/Go/Zobrist_hash.py
+++ http_server/Go/Zobrist_hash.py
@@ -1,5 +1,9 @@
 import numpy as np
-from go_types import State
+try:
+    from go_types import State
+except:
+    from typing import Any
+    State = np.ndarray[Any, np.dtype[np.int8]]
 
 
 class ZobristHash:
